0808f3ff0533f9f1aa488346e69ac8c2a3f8998e
Wasis Putranto <wasis.putranto@binance.com>
2020-12-05 20:44:45 +0800

    pytz

--- ratelimit/test.py
+++ ratelimit/test.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
+import datetime
+import pytz
+import dateutil.parser
 
-def convert_unix_timestamp(arg):
-    pass
+def convert_unix_timestamp(datestring):
+    format = '[%d/%b/%Y:%H:%M:%S%z]'
+    d = datetime.datetime.strptime(datestring, format)
+    #d = d.replace(tzinfo=utc) - d.utcoffset()
+    return d
 
 def parse_input(input_file):
     with open(input_file, 'r') as in_file:
         for line in in_file:
             log_list = line.split()
-            timestamp = log_list[3].strip("[")
+            log_timestamp = log_list[3] + log_list[4]
+            timestamp = convert_unix_timestamp(log_timestamp)
             ip = log_list[0]
             decision = "BAN"
             print(timestamp, decision, ip)
