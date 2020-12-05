ba44e10254c5a6b2407c02f21c1c7b736881a014
Wasis Putranto <wasis.putranto@binance.com>
2020-12-05 10:30:07 +0800

    try

--- flasktest/main.py
+++ flasktest/main.py
@@ -1,25 +1,9 @@
 from flask import Flask, request, render_template, redirect
-from math import floor
-from sqlite3 import OperationalError
 import string
-import sqlite3
-try:
-    from urllib.parse import urlparse  # Python 3
-    str_encode = str.encode
-except ImportError:
-    from urlparse import urlparse  # Python 2
-    str_encode = str
-try:
-    from string import ascii_lowercase
-    from string import ascii_uppercase
-except ImportError:
-    from string import lowercase as ascii_lowercase
-    from string import uppercase as ascii_uppercase
-import base64
+import random
 import time
 import redis
 
-# Assuming urls.db is in your app root folder
 app = Flask(__name__)
 host = 'http://192.168.77.10:5000/'
 cache = redis.Redis(host='redis', port=6379)
@@ -35,43 +19,17 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
-
-def table_check():
-    create_table = """
-        CREATE TABLE WEB_URL(
-        ID INT PRIMARY KEY AUTOINCREMENT,
-        URL TEXT NOT NULL
-        );
-        """
-    with sqlite3.connect('urls.db') as conn:
-        cursor = conn.cursor()
+def set_url(short_url, long_url):
+    retries = 5
+    while True:
         try:
-            cursor.execute(create_table)
-        except OperationalError:
-            pass
+            return cache.set(short_url, long_url)
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
-
-def toBase62(num, b=62):
-    if b <= 0 or b > 62:
-        return 0
-    base = string.digits + ascii_lowercase + ascii_uppercase
-    r = num % b
-    res = base[r]
-    q = floor(num / b)
-    while q:
-        r = q % b
-        q = floor(q / b)
-        res = base[int(r)] + res
-    return res
-
-
-def toBase10(num, b=62):
-    base = string.digits + ascii_lowercase + ascii_uppercase
-    limit = len(num)
-    res = 0
-    for i in range(limit):
-        res = b * res + base.find(num[i])
-    return res
 
 @app.route('/')
 def hello():
@@ -81,49 +39,26 @@ def hello():
 @app.route('/newurl', methods=['GET', 'POST'])
 def newurl():
     if request.method == 'POST':
-        original_url = str_encode(request.form.get('url'))
-        if urlparse(original_url).scheme == '':
-            url = 'http://' + original_url
-        else:
-            url = original_url
-        with sqlite3.connect('urls.db') as conn:
-            cursor = conn.cursor()
-            res = cursor.execute(
-                'INSERT INTO WEB_URL (URL) VALUES (?)',
-                [base64.urlsafe_b64encode(url)]
-            )
+        surl = short_url_generator()
+        lurl = str_encode(request.form.get('url'))
 
-            conn.commit()
+        set_url(surl, lurl)
 
-            encoded_string = toBase62(res.lastrowid)
-
-        return render_template('home.html', short_url=host + encoded_string)
+        return render_template('home.html', short_url = host + surl)
 
     return render_template('home.html')
 
 
 @app.route('/<short_url>')
 def redirect_short_url(short_url):
-    decoded = toBase10(short_url)
-    url = host  # fallback if no URL is found
+    lurl = host + "/newurl"  # fallback if no URL is found
 
     try:
-        with sqlite3.connect('urls.db') as conn:
-            cursor = conn.cursor()
-            res = cursor.execute('SELECT URL FROM WEB_URL WHERE ID=?', [decoded])
-            try:
-                short = res.fetchone()
-                if short is not None:
-                    url = base64.urlsafe_b64decode(short[0])
-            except Exception as e:
-                print(e)
-        return redirect(url)
-
+        lurl = cache.get(short_url)
+        return redirect(lurl)
     except OverflowError as e:
         print(str(e))
 
 
 if __name__ == '__main__':
-    # This code checks whether database table is created or not
-    table_check()
     app.run(debug=True)
