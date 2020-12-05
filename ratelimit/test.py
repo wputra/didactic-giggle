1584b8cef323ceb85c62aa7e0a7cd00c69f2e6c7
Wasis Putranto <wasis.putranto@binance.com>
2020-12-05 23:25:45 +0800

    Update test.py

--- ratelimit/test.py
+++ ratelimit/test.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
 import datetime
 import pytz
+import redis
 
 def convert_unix_timestamp(datestring):
     d = datetime.datetime.strptime(datestring, format)
     d = d.astimezone(utc)
     ts = (d - epoch).total_seconds()
     return int(ts)
+
+def request_is_limited(t, key, limit, period):
+    period_in_seconds = int(period.total_seconds())
+    separation = round(period_in_seconds / limit)
+    r.setnx(key, 0)
+    try:
+        with r.lock('lock:' + key, blocking_timeout=5) as lock:
+            tat = max(int(r.get(key)), t)
+            if tat - t <= period_in_seconds - separation:
+                new_tat = max(tat, t) + separation
+                r.set(key, new_tat)
+                return False
+            return True
+    except LockError:
+        return True
 
 def parse_input(input_file):
     with open(input_file, 'r') as in_file:
@@ -15,13 +31,27 @@ def parse_input(input_file):
             log_timestamp = log_list[3] + log_list[4]
             timestamp = convert_unix_timestamp(log_timestamp)
             ip = log_list[0]
+
+            if request_is_limited(timestamp, ip, 40, datetime.timedelta(minutes=1)):
+                decision = "BAN"
+            elif request_is_limited(timestamp, ip, 100, datetime.timedelta(minutes=10)):
+                decision = "BAN"
+            else:
+                decision = "UNBAN"
+
             path = log_list[6]
-            decision = "BAN"
-            print(f'{timestamp},{decision},{ip},{path}')
+            if path == "/login":
+                if request_is_limited(timestamp, ip+"_login", 20, datetime.timedelta(minutes=10)):
+                    decision = "BAN"
+                else:
+                    decision = "UNBAN"
+
+            print(f'{timestamp},{decision},{ip}')
 
 
 # main
 format = '[%d/%b/%Y:%H:%M:%S%z]'
 utc = pytz.UTC
 epoch = datetime.datetime(1970,1,1,0,0,0,tzinfo=utc)
+r = redis.Redis(host='localhost', port=6379, db=0)
 parse_input("test.log")
